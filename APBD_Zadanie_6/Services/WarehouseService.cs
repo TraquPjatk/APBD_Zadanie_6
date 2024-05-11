1d560cb0c39222818b35ba64eed1487b3e85b0af
TraquPjatk <s19787@pjwstk.edu.pl>
2024-05-11 20:49:27 +0200

    fix

--- APBD_Zadanie_6/Services/WarehouseService.cs
+++ APBD_Zadanie_6/Services/WarehouseService.cs
@@ -1,4 +1,5 @@
-﻿using APBD_Task_6.Models;
+﻿using System.Data;
+using APBD_Task_6.Models;
 using System.Data.SqlClient;
 
 namespace Zadanie5.Services
@@ -23,35 +24,50 @@ namespace Zadanie5.Services
                 var command = new SqlCommand();
                 command.Connection = connection; // mam połączenie UwU
 
+                try
+                {
+                
+                    if (productExists(product.IdProduct, connection))
+                    {
+                        throw new AggregateException("Product with id: " + product.IdProduct + " exists.");
+                    }
+
+                    if (warehouseExists(product.IdWarehouse, connection))
+                    {
+                        throw new AggregateException("Warehouse with id: " + product.IdWarehouse + " exists.");
+                    }
+
+                    if (product.Amount <= 0)
+                    {
+                        throw new AggregateException("Product amount should be grater than 0!");
+                    }
+
+                    _productsInWarehouse.Add(product);
+                    command.CommandText = "" +
+                                          "INSERT INTO Product_Warehouse (IdProduct, IdWarehouse, Amount, CreatedAt) " +
+                                          "VALUES (@IdProduct, @IdWarehouse, @Amount, @CreatedAt)";
+
+                    command.Parameters.AddWithValue("@IdProduct", product.IdProduct);
+                    command.Parameters.AddWithValue("@IdWarehouse", product.IdWarehouse);
+                    command.Parameters.AddWithValue("@Amount", product.Amount);
+                    command.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+
+                    command.ExecuteNonQuery();
+
+                    _warehouseService.AddProduct(product);
                     
-                if (productExists(product.IdProduct, connection))
-                {
-                    throw new AggregateException("Product with id: " + product.IdProduct + " exists.");
                 }
-
-                if (warehouseExists(product.IdWarehouse, connection))
+                catch (Exception e)
                 {
-                    throw new AggregateException("Warehouse with id: " + product.IdWarehouse + " exists.");
+                    Console.Write(e.Message);
                 }
-
-                if (product.Amount <= 0)
+                finally
                 {
-                    throw new AggregateException("Product amount should be grater than 0!");
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
                 }
-
-                _productsInWarehouse.Add(product);
-                command.CommandText = "" +
-                                      "INSERT INTO Product_Warehouse (IdProduct, IdWarehouse, Amount, CreatedAt) " +
-                                      "VALUES (@IdProduct, @IdWarehouse, @Amount, @CreatedAt)";
-
-                command.Parameters.AddWithValue("@IdProduct", product.IdProduct);
-                command.Parameters.AddWithValue("@IdWarehouse", product.IdWarehouse);
-                command.Parameters.AddWithValue("@Amount", product.Amount);
-                command.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
-
-                command.ExecuteNonQuery();
-
-                _warehouseService.AddProduct(product);
                 // _productsInWarehouse.Add(product);
             }
         }
@@ -62,13 +78,13 @@ namespace Zadanie5.Services
             command.Parameters.AddWithValue("@IdProduct", productId);
             connection.Open();
             int count = (int)command.ExecuteScalar(); //to mi wykonuje select
-
+            
             return count > 0;
         }
 
         private bool warehouseExists(int warehouseId, SqlConnection connection)
         {
-            using var command = new SqlCommand("SELECT COUNT(*) FROM Product WHERE IdWarehouse = @IdWarehouse",
+            using var command = new SqlCommand("SELECT COUNT(*) FROM Warehouse WHERE IdWarehouse = @IdWarehouse",
                 connection);
             command.Parameters.AddWithValue("@IdWarehouse", warehouseId);
             connection.Open();
