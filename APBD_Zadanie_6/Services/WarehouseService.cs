a201c80c9009462874b27f119aa9411c3d957e48
TraquPjatk <s19787@pjwstk.edu.pl>
2024-05-11 20:29:23 +0200

    2

--- APBD_Zadanie_6/Services/WarehouseService.cs
+++ APBD_Zadanie_6/Services/WarehouseService.cs
@@ -18,45 +18,48 @@ namespace Zadanie5.Services
         public void AddProduct(ProductWarehouse product)
         {
             var connectionString = _configuration.GetConnectionString("Database");
-            using var connection = new SqlConnection(connectionString);
-            var command = new SqlCommand();
-            command.Connection = connection; //mam połączenie UwU
-
-            if (productExists(product.IdProduct, connection))
+            using (var connection = new SqlConnection(connectionString))
             {
-                throw new AggregateException("Product with id: " + product.IdProduct + " exists.");
+                var command = new SqlCommand();
+                command.Connection = connection; // mam połączenie UwU
+
+                    
+                if (productExists(product.IdProduct, connection))
+                {
+                    throw new AggregateException("Product with id: " + product.IdProduct + " exists.");
+                }
+
+                if (warehouseExists(product.IdWarehouse, connection))
+                {
+                    throw new AggregateException("Warehouse with id: " + product.IdWarehouse + " exists.");
+                }
+
+                if (product.Amount <= 0)
+                {
+                    throw new AggregateException("Product amount should be grater than 0!");
+                }
+
+                _productsInWarehouse.Add(product);
+                command.CommandText = "" +
+                                      "INSERT INTO Product_Warehouse (IdProduct, IdWarehouse, Amount, CreatedAt) " +
+                                      "VALUES (@IdProduct, @IdWarehouse, @Amount, @CreatedAt)";
+
+                command.Parameters.AddWithValue("@IdProduct", product.IdProduct);
+                command.Parameters.AddWithValue("@IdWarehouse", product.IdWarehouse);
+                command.Parameters.AddWithValue("@Amount", product.Amount);
+                command.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
+
+                command.ExecuteNonQuery();
+
+                _warehouseService.AddProduct(product);
+                // _productsInWarehouse.Add(product);
             }
-
-            if (warehouseExists(product.IdWarehouse, connection))
-            {
-                throw new AggregateException("Warehouse with id: " + product.IdWarehouse + " exists.");
-            }
-
-            if (product.Amount <= 0)
-            {
-                throw new AggregateException("Product amount should be grater than 0!");
-            }
-
-            _productsInWarehouse.Add(product);
-            command.CommandText = "" +
-                                  "INSERT INTO ProductWarehouse (IdProduct, IdWarehouse, Amount, CreatedAt) " +
-                                  "VALUES (@IdProduct, @IdWarehouse, @Amount, @CreatedAt)";
-
-            command.Parameters.AddWithValue("@IdProduct", product.IdProduct);
-            command.Parameters.AddWithValue("@IdWarehouse", product.IdWarehouse);
-            command.Parameters.AddWithValue("@Amount", product.Amount);
-            command.Parameters.AddWithValue("@CreatedAt", DateTime.Now);
-
-            command.ExecuteNonQuery();
-
-            _warehouseService.AddProduct(product);
-            // _productsInWarehouse.Add(product);
         }
 
         private bool productExists(int productId, SqlConnection connection)
         {
-            using var command = new SqlCommand("SELECT COUNT(*) FROM Products WHERE Id = @ProductId", connection);
-            command.Parameters.AddWithValue("@ProductId", productId);
+            using var command = new SqlCommand("SELECT COUNT(*) FROM Product WHERE IdProduct = @IdProduct", connection);
+            command.Parameters.AddWithValue("@IdProduct", productId);
             connection.Open();
             int count = (int)command.ExecuteScalar(); //to mi wykonuje select
 
@@ -65,8 +68,9 @@ namespace Zadanie5.Services
 
         private bool warehouseExists(int warehouseId, SqlConnection connection)
         {
-            using var command = new SqlCommand("SELECT COUNT(*) FROM Products WHERE Id = @WarehouseId", connection);
-            command.Parameters.AddWithValue("@WarehouseId", warehouseId);
+            using var command = new SqlCommand("SELECT COUNT(*) FROM Product WHERE IdWarehouse = @IdWarehouse",
+                connection);
+            command.Parameters.AddWithValue("@IdWarehouse", warehouseId);
             connection.Open();
             int count = (int)command.ExecuteScalar(); //to mi wykonuje select
 
